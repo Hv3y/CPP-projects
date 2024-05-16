04e0bfebd33874da8ceb49675663e9e5c9600d6a
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 15:44:45 +0200

    Add files via upload

---
+++ WindowsApplication6/WindowsApplication6/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number As Integer = TextBox1.Text
+        Dim Total As Integer
+        For i = 0 To 12 Step 1
+            Total = number * 1
+            ListBox1.Items.Add(number.ToString & "x" & i.ToString & "=" & Total.ToString)
+        Next
+       
+    End Sub
+End Class
