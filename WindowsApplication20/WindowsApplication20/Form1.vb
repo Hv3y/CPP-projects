2a3b872c6eebd438b4f5f05aa1e5d150b49c0ddb
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 16:27:08 +0200

    Add files via upload

---
+++ WindowsApplication20/WindowsApplication20/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Name As String = TextBox1.Text
+        Dim Counter As Integer = TextBox2.Text
+        For x = 0 To Counter - 1 Step 1
+            ListBox1.Items.Add("Your name is:" + Name)
+        Next
+    End Sub
+End Class
