c5a23095a49cc239196b33c5303e0e55a2000437
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 15:43:02 +0200

    Add files via upload

---
+++ WindowsApplication4/WindowsApplication4/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(Txtname.Text & vbTab & Txtemail.Text)
+    End Sub
+
+    Private Sub Txtname_TextChanged(sender As Object, e As EventArgs) Handles Txtname.TextChanged
+
+    End Sub
+End Class
