e58b6c5b5f6acf46161645d44f29f738b4be54f2
Lesedi Seema <156032629+Hv3y@users.noreply.github.com>
2024-05-16 15:37:50 +0200

    Add files via upload

---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim radius As Decimal = TextBox1.Text
+        Dim height As Decimal = TextBox2.Text
+        Dim Pie As Decimal = 3.1459
+        Dim Volume As Decimal = Pie * (radius * radius) * height
+        Dim Surface As Decimal = 2 * (Pie * (radius)) + 2 * (Pie * radius * height)
+        MessageBox.Show("The volume of cylinder is:" & Decimal.Round(Volume, 2).ToString & "to decimal places" & vbNewLine)
+    End Sub
+End Class
